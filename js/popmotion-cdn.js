a0060e0930fd1f9776366ab07e451ed41a12cc91
p49 <logindivpro@gmail.com>
2019-02-18 09:28:34 -0500

    initial commit for popmotion-cdn

---
+++ js/popmotion-cdn.js
@@ -0,0 +1,22 @@
+const {styler, spring, listen, pointer, value} = window.popmotion;
+
+const ball = document.querySelector(".box");
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0}, divStyler.set);
+
+listen(ball, "mousedown touchstart").start(function(e) {
+	  e.preventDefault();
+	  pointer(ballXY.get()).start(ballXY);
+  });
+  
+ listen(document, "mouseup touchend").start(function() {
+	 spring({
+		 from: ballXY.get(),
+		 velocity: ballXY.getVelocity(),
+		 to: {x: 0, y: 0},
+		 stiffness: 200
+		   //mass: 1'
+		   //damping: 10
+	 }).start(ballXY);
+ });
+ 
